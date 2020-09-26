2ba3cbfe652a9c77caca747765fea4e5d0808c09
NearLawiet <sooraj1999vs@gmail.com>
2020-09-26 13:47:04 +0530

    added connection and conversation tables

--- procedures.sql
+++ procedures.sql
@@ -1,28 +1,95 @@
-create procedure issue_enrollment_forms()
-begin
-    declare cur_applicant_id varchar(16);
-    declare has_enrollment_form int default 0;
-    declare end_of_table bool default false;
+create table hsc(
+    hsc_reg_no varchar(8) not null primary key,
+    board varchar(16) not null,
+    percentage double not null
+);
 
-    declare record_pointer cursor for
-        select applicant_id from applicant where status='LOCKED';
+create table entrance(
+    entrance_reg_no varchar(8) not null primary key,
+    percentile double not null,
+    score double not null
+);
 
-    declare continue handler for not found
-    set end_of_table = true;
+create table application_form(
+    unique_id varchar(16) not null primary key ,
+    id_type enum('UIDAI','PASSPORT','PAN','DRIVING_LICENSE','VOTER_ID') default 'UIDAI',
+    first_name varchar(16) not null,
+    middle_name varchar(16),
+    last_name varchar(16) not null,
+    email varchar(64) not null,
+    phone varchar(14) not null,
+    category enum ('GEN','OBC','SC/ST','NT') default 'GEN',
+    entrance_reg_no varchar(8) not null unique ,
+    hsc_reg_no varchar(8) not null unique ,
+    branch_name varchar(8) not null
+);
 
-    open record_pointer;
-         fetch record_pointer into cur_applicant_id;
-        while end_of_table = false do
-            select count(*) from enrollment_form where applicant_id=cur_applicant_id
-            into has_enrollment_form;
+create table applicant(
+    applicant_id varchar(16) not null primary key,
+    enrollment_id varchar(16) unique,
+    password varchar(16) not null,
+    unique_id varchar(16) not null unique ,
+    status enum ('PENDING','APPLIED','SHORTLISTED','LOCKED','REJECTED','FLOATED','UNDER_VERIFICATION','ENROLLED','NOT_FOUND','FAILED') not null default 'PENDING'
 
-            if has_enrollment_form = 0 then
-                insert into enrollment_form(applicant_id, placeholder)
-                value (cur_applicant_id,'EMPTY');
-            end if;
 
-            fetch record_pointer into cur_applicant_id;
-        end while;
-    close record_pointer;
-end;
+);
+
+create table enrollment_form(
+    applicant_id varchar(16) not null primary key ,
+    placeholder varchar(16) not null
+
+);
+
+create table admin(
+    username varchar(16) primary key,
+    name varchar(32),
+    password varchar(16) not null
+);
+
+create table connection(
+    ticket_no varchar(16) primary key not null,
+    username varchar(16),
+    client_name varchar(32) not null,
+    applicant_id varchar(16)
+);
+
+create table conversation(
+    id int primary key not null auto_increment,
+    message text not null,
+    ticket_no varchar(16) not null,
+    sent_at timestamp not null,
+    sent_by_admin bool not null default(false)
+);
+
+alter table application_form add constraint application_form_FK_hsc_reg_no
+foreign key(hsc_reg_no) references hsc(hsc_reg_no)
+on delete cascade;
+
+alter table application_form add constraint application_form_FK_entrance_reg_no
+foreign key(entrance_reg_no) references entrance(entrance_reg_no)
+on delete cascade;
+
+alter table applicant add constraint applicant_FK_unique_id
+foreign key(unique_id) references application_form(unique_id)
+on delete cascade;
+
+
+alter table enrollment_form add constraint enrollment_form_FK_applicant_id
+foreign key(applicant_id) references applicant(applicant_id)
+on delete cascade;
+
+alter table connection add constraint connection_FK_username
+foreign key(username) references admin(username)
+on delete set null;
+
+alter table connection add constraint connection_FK_applicant_id
+foreign key(applicant_id) references applicant(applicant_id)
+on delete cascade;
+
+alter table conversation add constraint conversation_FK_ticket_no
+foreign key(ticket_no) references connection(ticket_no)
+on delete cascade;
+
+alter table conversation auto_increment = 0;
+alter table conversation modify column sent_at timestamp not null default (current_timestamp);
 
